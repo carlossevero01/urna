3862fdfdd80f6bb5336945432a4db08631898747
carlossevero01 <carlos212427@gmail.com>
2022-12-14 16:51:41 -0300

    create project

---
+++ urnagaucha/src/main/java/com/ifsul/urnagaucha/urnagaucha/UrnagauchaApplication.java
@@ -0,0 +1,13 @@
+package com.ifsul.urnagaucha.urnagaucha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrnagauchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrnagauchaApplication.class, args);
+	}
+
+}
---
+++ urnagaucha/src/test/java/com/ifsul/urnagaucha/urnagaucha/UrnagauchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsul.urnagaucha.urnagaucha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrnagauchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
